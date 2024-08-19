5e61f0479f8f39b8d742bb085020d848a97ad66c
yjson <handyejee14@gmail.com>
2024-08-19 20:59:00 +0900

    first commit

---
+++ src/main/java/livoi/swimdiary/SwimdiaryApplication.java
@@ -0,0 +1,13 @@
+package livoi.swimdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwimdiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwimdiaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/livoi/swimdiary/SwimdiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package livoi.swimdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwimdiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
