9f2970356fafe3d18c5a54fe76259d8cf6191f3f
yjson <handyejee14@gmail.com>
2024-08-28 11:05:54 +0900

    feat: MindDiary repository interface 정의

---
+++ src/main/java/livoi/swimdiary/repository/MindDiaryRepository.java
@@ -0,0 +1,8 @@
+package livoi.swimdiary.repository;
+
+import livoi.swimdiary.domain.MindDiary;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MindDiaryRepository extends JpaRepository<MindDiary, Long> {
+
+ }
