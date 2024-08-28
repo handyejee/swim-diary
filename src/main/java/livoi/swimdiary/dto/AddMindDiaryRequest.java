0abea63eb4b6748c0035b025f3f197b391c3630d
yjson <handyejee14@gmail.com>
2024-08-28 11:08:08 +0900

    feat: MindDiary 등록 DTO 추가

---
+++ src/main/java/livoi/swimdiary/dto/AddMindDiaryRequest.java
@@ -0,0 +1,43 @@
+package livoi.swimdiary.dto;
+
+import java.time.LocalDate;
+import livoi.swimdiary.domain.MindDiary;
+import livoi.swimdiary.domain.Users;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ *  감정일기 DTO
+ *  감정 일기 등록시 필요한 항목들을 MoodDiary Entity와 매핑하는 DTO
+ */
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class AddMindDiaryRequest {
+
+  private Long diaryId;
+  private Users users;
+  private String workoutMood;
+  private String workoutType;
+  private Integer workoutCount;
+  private String workoutLog;
+  private LocalDate createdAt;
+  private LocalDate modifiedAt;
+  private LocalDate deletedAt;
+
+  public MindDiary toEntity(){
+    return MindDiary.builder()
+        .diaryId(diaryId)
+        .users(users)
+        .workoutMood(workoutMood)
+        .workoutType(workoutType)
+        .workoutCount(workoutCount)
+        .workoutLog(workoutLog)
+        .createdAt(createdAt)
+        .modifiedAt(modifiedAt)
+        .deletedAt(deletedAt)
+        .build();
+  }
+}
