b0c2f33ac241e310e31980de6773edf598159eaa
yjson <handyejee14@gmail.com>
2024-09-04 16:45:48 +0900

    applyReview: Response dto를 추가해 fromEntity 메서드를 사용해 엔티티를 직접 리턴하지 않도록 개선

--- src/main/java/livoi/swimdiary/controller/DiaryApiController.java
+++ src/main/java/livoi/swimdiary/controller/DiaryApiController.java
@@ -2,10 +2,12 @@ package livoi.swimdiary.controller;
 
 import livoi.swimdiary.domain.Diary;
 import livoi.swimdiary.dto.AddDiaryRequest;
+import livoi.swimdiary.dto.DiaryResponseDto;
 import livoi.swimdiary.service.DiaryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +19,12 @@ public class DiaryApiController {
   private final DiaryService diaryService;
 
   @PostMapping("/mind-diary")
-  public ResponseEntity<Diary> addMoodDiary(@RequestBody AddDiaryRequest request){
+  public ResponseEntity<DiaryResponseDto> addMoodDiary(@RequestBody @Validated AddDiaryRequest request){
     Diary savedDiary = diaryService.save(request);
+    DiaryResponseDto responseDto = DiaryResponseDto.fromEntity(savedDiary);
 
     return ResponseEntity.status(HttpStatus.CREATED)
-        .body(savedDiary);
+        .body(responseDto);
   }
 
 }
