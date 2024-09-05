572461e189a302d96a802023cd3e7befb3bd6a8c
yjson <handyejee14@gmail.com>
2024-09-05 22:25:53 +0900

    fix : 서버에서 엔티티 항목 전송시 클라이언트에서 불필요한 userId, modifiedAt, deletedAt 항목 제거

--- src/main/java/livoi/swimdiary/dto/AddDiaryResponseDto.java
+++ src/main/java/livoi/swimdiary/dto/AddDiaryResponseDto.java
@@ -6,32 +6,36 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+/**
+ * 신규 등록한 감정일기 항목에 대한 응답 데이터를 나타내는 클래스 입니다.
+ * 이 DTO는 감정일기를 등록하는 REST API의 호출의 응답으로 반환됩니다.
+ */
 @Getter
 @AllArgsConstructor
 @Builder
-public class DiaryResponseDto {
+public class AddDiaryResponseDto {
 
   private final Long diaryId;
-  private final Long userId;
   private final String workoutMood;
   private final String workoutType;
   private final Integer workoutCount;
   private final String workoutLog;
   private final LocalDate createdAt;
-  private final LocalDate modifiedAt;
-  private final LocalDate deletedAt;
 
-  public static DiaryResponseDto fromEntity(Diary diary) {
-    return DiaryResponseDto.builder()
+  /**
+   * Diary 엔티티를 AddDiaryResponseDto로 변환합니다.
+   *
+   * @param diary 변환할 Diary 엔티티 입니다.
+   * @return Diary 엔티티에서 추출된 데이터를 AddDiaryResponse 객체로 변환한 후 그 객체를 반환합니다.
+   */
+  public static AddDiaryResponseDto fromEntity(Diary diary) {
+    return AddDiaryResponseDto.builder()
         .diaryId(diary.getDiaryId())
-        .userId(diary.getUsers().getUserId())
         .workoutMood(diary.getWorkoutMood())
         .workoutType(diary.getWorkoutType())
         .workoutCount(diary.getWorkoutCount())
         .workoutLog(diary.getWorkoutLog())
         .createdAt(diary.getCreatedAt())
-        .modifiedAt(diary.getModifiedAt())
-        .deletedAt(diary.getDeletedAt())
         .build();
   }
 }
