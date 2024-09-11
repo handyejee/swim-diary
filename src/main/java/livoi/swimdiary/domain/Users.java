063cc61f5e88aec3fb713f280c531ef991bdd59f
yjson <handyejee14@gmail.com>
2024-09-11 16:42:54 +0900

    feat : Spring Security 인증정보 사용을 위한 오버라이드 메서드 추가

--- src/main/java/livoi/swimdiary/domain/Users.java
+++ src/main/java/livoi/swimdiary/domain/Users.java
@@ -5,17 +5,23 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-
+@Table(name = "users")
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Users {
+public class Users implements UserDetails {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,23 +31,59 @@ public class Users {
   @Column(name = "username", nullable = false)
   private String username;
 
-  @Column(name = "email", nullable = false)
+  @Column(name = "email", nullable = false, unique = true)
   private String email;
 
   @Column(name = "password", nullable = false)
   private String password;
 
+  @Column(name = "nickname")
   private String nickname;
 
   @Column(name = "swim_routine")
   private Integer swimRoutine;
 
   @Builder
-  public Users(String username, String email, String password, String nickname) {
+  public Users(String username, String email, String password, String nickname, Integer swimRoutine, String auth) {
     this.username = username;
     this.email = email;
     this.password = password;
     this.nickname = nickname;
+    this.swimRoutine = swimRoutine;
   }
 
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of(new SimpleGrantedAuthority("users"));
+  }
+
+  @Override
+  public String getUsername(){
+    return email;
+  }
+
+  @Override
+  public String getPassword(){
+    return password;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return UserDetails.super.isAccountNonExpired();
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return UserDetails.super.isAccountNonLocked();
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return UserDetails.super.isCredentialsNonExpired();
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return UserDetails.super.isEnabled();
+  }
 }
