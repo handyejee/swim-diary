b883df8725812558ad1c9517cb49fcb298530c1f
yjson <handyejee14@gmail.com>
2024-09-16 13:43:15 +0900

    fix : 사용자 정보 조회시 명확한 예외 발생하도록 변경

--- src/main/java/livoi/swimdiary/service/UserDetailService.java
+++ src/main/java/livoi/swimdiary/service/UserDetailService.java
@@ -4,14 +4,14 @@ import livoi.swimdiary.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
 /**
  * 사용자 정보를 조회하기 위한 서비스 로직
  * UserDetailsService 인터페이스를 구현합니다.
  */
-@RequiredArgsConstructor
 @Service
+@RequiredArgsConstructor
 public class UserDetailService implements UserDetailsService {
 
   private final UsersRepository usersRepository;
@@ -20,11 +20,12 @@ public class UserDetailService implements UserDetailsService {
    * 이메일로 사용자를 검색합니다.
    * @param email 사용자의 이메일
    * @return userRepository에서 검색한 이메일 반환.
-   * @throws IllegalArgumentException 사용자 이메일 찾을 수 없을 경우 예외 발생
+   * @throws UsernameNotFoundException 사용자 이메일 찾을 수 없을 경우 예외 발생
    */
+
   @Override
-  public UserDetails loadUserByUsername(String email){
+  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
     return usersRepository.findByEmail(email)
-        .orElseThrow(() -> new IllegalArgumentException("User email not found" + email));
+        .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
   }
 }
