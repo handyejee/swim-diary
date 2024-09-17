4a5beabc718f2fdec05d3160c94b03c9bbc4c1b0
yjson <handyejee14@gmail.com>
2024-09-18 00:03:24 +0900

    feat : 이메일로 사용자 조회시 boolean 타입으로 확인하는 메서드 추가

--- src/main/java/livoi/swimdiary/repository/UsersRepository.java
+++ src/main/java/livoi/swimdiary/repository/UsersRepository.java
@@ -11,9 +11,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UsersRepository extends JpaRepository<Users, Long> {
 
   /**
-   * 이메일로 사용자를 검색합니다.
+   * 전달받은 이메일로 등록된 사용자가 있는지 조회하는 메서드
+   *
    * @param email 사용자의 이메일
    * @return 이메일과 일치하는 사용자가 존재할 경우 Users 객체 반환
    */
   Optional<Users> findByEmail(String email);
+
+  /**
+   * 전달받은 이메일로 등록된 사용자가 있는지 조회하고 boolean으로 결과 값을 주는 메서드
+   *
+   * @param email 사용자의 이메일
+   * @return 사용자 존재여부 boolean 타입으로 반환
+   */
+  boolean existsByEmail(String email);
 }
