873559de9d48fc487944151412c9d70076ba6f5d
yjson <handyejee14@gmail.com>
2024-09-18 00:09:19 +0900

    fix : 컨트롤러에서 검증시 Valid 어노테이션으로 검증하도록 수정

--- src/main/java/livoi/swimdiary/controller/UserApiController.java
+++ src/main/java/livoi/swimdiary/controller/UserApiController.java
@@ -1,5 +1,6 @@
 package livoi.swimdiary.controller;
 
+import jakarta.validation.Valid;
 import livoi.swimdiary.domain.Users;
 import livoi.swimdiary.dto.AddUserRequest;
 import livoi.swimdiary.dto.AddUserResponseDto;
@@ -8,7 +9,6 @@ import livoi.swimdiary.service.UsersService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,7 +25,7 @@ public class UserApiController {
   private final UsersService usersService;
 
   @PostMapping("/signup")
-  public ResponseEntity<AddUserResponseDto> signup(@RequestBody @Validated AddUserRequest request){
+  public ResponseEntity<AddUserResponseDto> signup(@RequestBody @Valid AddUserRequest request){
     Users savedUsers = usersService.saveUser(request);
     AddUserResponseDto userResponseDto = AddUserResponseDto.fromEntity(savedUsers);
 
@@ -34,8 +34,9 @@ public class UserApiController {
   }
 
   @PostMapping("/login")
-  public ResponseEntity<String> login(@RequestBody LoginUserRequest loginUserRequest){
+  public ResponseEntity<String> login(@RequestBody @Valid LoginUserRequest loginUserRequest){
     String token = usersService.signIn(loginUserRequest);
+
     return ResponseEntity.ok(token);
   }
 
