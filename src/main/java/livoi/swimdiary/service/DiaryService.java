281052067378cb8858d65efb0ed234fea4b9350b
yjson <handyejee14@gmail.com>
2024-09-22 23:48:51 +0900

    feat : 감정일기 생성 시작,종료 날짜 기준으로 조회하는 서비스 로직 구현

--- src/main/java/livoi/swimdiary/service/DiaryService.java
+++ src/main/java/livoi/swimdiary/service/DiaryService.java
@@ -1,8 +1,12 @@
 package livoi.swimdiary.service;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 import livoi.swimdiary.domain.Diary;
 import livoi.swimdiary.domain.Users;
 import livoi.swimdiary.dto.AddDiaryRequest;
+import livoi.swimdiary.dto.GetDiaryResponseDto;
 import livoi.swimdiary.dto.UpdateDiaryRequest;
 import livoi.swimdiary.dto.UpdateDiaryResponseDto;
 import livoi.swimdiary.repository.DiaryRepository;
@@ -10,6 +14,7 @@ import livoi.swimdiary.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * 감정일기 기능을 위한 서비스 로직
@@ -58,12 +63,40 @@ public class DiaryService {
   }
 
   /**
-   * 감정일기를 삭제합니다.
+   * 사용자가 선택한 날짜 기준으로 감정일기를 조회합니다.
    *
-   * @param diaryId 감정일기의 diaryId 값을 받아옵니다.
+   * @param startDate 검색 시작 일자
+   * @param endDate 검색 종료 일자
+   * @return 감정일기 목록을 조회한 Diary 엔티티의 GetDiaryResponseDto를 리스트로 변환해 반환
    */
-  public void delete(long diaryId) {
-    diaryRepository.deleteById(diaryId);
+  public List<GetDiaryResponseDto> getDiaryByDate(LocalDate startDate, LocalDate endDate) {
+    validateDate(startDate, endDate);
+    List<Diary> diaries = diaryRepository.findAllByCreatedAtBetween(startDate, endDate);
+
+    return diaries.stream()
+        .map(GetDiaryResponseDto::fromEntity)
+        .collect(Collectors.toList());
   }
+
+  /**
+   * 검색 시작일이 검색 종료일보다 빠른 날짜일 경우 예외가 발생합니다.
+   *
+   * @param startDate 검색 시작 일자
+   * @param endDate 검색 종료 일자
+   */
+  public void validateDate(LocalDate startDate, LocalDate endDate){
+    if (startDate.isAfter(endDate)) {
+      throw new IllegalArgumentException("Start date cannot be after end date.");
+    }
+  }
+
+/**
+ * 감정일기를 삭제합니다.
+ *
+ * @param diaryId 감정일기의 diaryId 값을 받아옵니다.
+ */
+public void delete(long diaryId) {
+  diaryRepository.deleteById(diaryId);
+}
 
 }
