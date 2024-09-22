584740a8daeb854ea08f6a0bdf0478f269985ac4
yjson <handyejee14@gmail.com>
2024-09-22 23:51:02 +0900

    feat : 감정일기를 시작, 종료 기준으로 조회하기 위한 컨트롤러 메서드 추가

--- src/main/java/livoi/swimdiary/controller/DiaryApiController.java
+++ src/main/java/livoi/swimdiary/controller/DiaryApiController.java
@@ -1,21 +1,28 @@
 package livoi.swimdiary.controller;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.List;
 import livoi.swimdiary.domain.Diary;
 import livoi.swimdiary.dto.AddDiaryRequest;
 import livoi.swimdiary.dto.AddDiaryResponseDto;
+import livoi.swimdiary.dto.GetDiaryResponseDto;
 import livoi.swimdiary.dto.UpdateDiaryRequest;
 import livoi.swimdiary.dto.UpdateDiaryResponseDto;
 import livoi.swimdiary.service.DiaryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -43,6 +50,16 @@ public class DiaryApiController {
     return ResponseEntity.ok().body(updatedDiary);
   }
 
+  @GetMapping("/mind-diaries")
+  public ResponseEntity<List<GetDiaryResponseDto>> getDiaries(
+      @RequestParam @NotNull @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+      @RequestParam @NotNull @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate
+  ) {
+    List<GetDiaryResponseDto> diaries = diaryService.getDiaryByDate(startDate, endDate);
+
+    return ResponseEntity.ok(diaries);
+  }
+
   @DeleteMapping("mind-diary/{diaryId}")
   public ResponseEntity<Void> deleteDiary(@PathVariable long diaryId) {
     diaryService.delete(diaryId);
@@ -50,5 +67,4 @@ public class DiaryApiController {
     return ResponseEntity.ok()
         .build();
   }
-
 }
